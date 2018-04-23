bb333f485f1fc3259f1df5280f7c00e01aa12fab
koala-v <1904579228@qq.com>
2018-04-23 15:54:14 +0800

    update all font size,weight

--- WMS/www/js/config.js
+++ WMS/www/js/config.js
@@ -29,7 +29,7 @@ appConfig.constant('ENV', {
     updateFile:   'update.json',
     rootPath:     'onhandPath',
     configFile:   'config.txt',
-    version:      '1.0.1.41',
+    version:      '1.0.1.44',
     parameter: {
         showSerialNo : false
     },
